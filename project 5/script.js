e68bedb48fc82a97fb63e3af9dc25228569f332b
MrMaxing <fonparinya24@gmail.com>
2024-03-27 10:19:41 +0700

    update

--- project 5/script.js
+++ project 5/script.js
@@ -1,7 +1,8 @@
 /**
- * @param {strig} id
- * @param {strig} path
- * @param {boolean} css
+ * @param {string} id - ID of the element
+ * @param {string} path - Path of the HTML
+ * @param {boolean} css - If the HTML is CSS
+ * @returns {Promise<void>} - Promise that resolves when the HTML is loaded
  */
 async function loadHtml(id, path, css = true) {
   const element = document.querySelector(`#${id}`);
@@ -23,11 +24,9 @@ async function loadHtml(id, path, css = true) {
 (async () => {
   await loadHtml("navbar", "./components/navbar/index.html");
   await loadHtml("header", "./components/header/index.html");
-  
   await loadHtml("content", "./components/content/index.html");
-  await loadHtml("content-side", "./components/content/side.html");
-  await loadHtml("content-main", "./components/content/main/side1.html", false);
-  
   await loadHtml("footer", "./components/footer/index.html");
-  
+  await loadHtml("content-side", "./components/content/side.html");
+
+  await loadHtml("content-main", "./routes/side1.html", false);
 })()
