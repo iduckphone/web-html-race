e54080ca72959aaf77d8e5d65b8bccfc0d6332fd
MrMaxing <fonparinya24@gmail.com>
2024-03-27 15:40:36 +0700

    update

--- project 5/script.js
+++ project 5/script.js
@@ -4,29 +4,34 @@
  * @param {boolean} css - If the HTML is CSS
  * @returns {Promise<void>} - Promise that resolves when the HTML is loaded
  */
-async function loadHtml(id, path, css, sticky = false) {
+async function loadHtml(id, path, css) {
   const element = document.querySelector(`#${id}`);
-  if (!element)
-    return console.error("Cannot Found Element")
+  if (!element) return console.error("Cannot Found Element");
   try {
     const response = await fetch(path);
     const html = await response.text();
     if (css) {
       const style = path.replace(path.split("/").pop(), "style.css");
-      if (sticky) {
-        element.outerHTML = html.replace("{path}", style)
-        element.outerHTML = html.replace("{path}", style)
-      }
-      else {
-        element.innerHTML = html.replace("{path}", style)
-        element.innerHTML = html.replace("{path}", style)
-      }
-    } else
-      element.innerHTML = html
+      element.innerHTML = html.replace("{path}", style);
+    } else {
+      element.innerHTML = html;
+      localStorage.removeItem("path");
+      localStorage.setItem("path", path);
+    }
   } catch (error) {
     console.error("Error loading HTML:", error);
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const path = localStorage.getItem("path");
+  if (path) {
+    setTimeout(() => {
+      loadHtml("content-main", path, false);
+      localStorage.removeItem("path");
+    }, 50)
+  }
+});
 
 (async () => {
   await loadHtml("navbar", "./components/navbar/index.html", true, true);
